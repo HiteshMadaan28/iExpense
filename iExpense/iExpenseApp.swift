6b3689d64ede3b402db72beaf18b99d1ef36532d
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-05-23 22:58:05 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Hitesh Madaan on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
